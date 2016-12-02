ec2e1aa82699c808384b05b961421208601aaa57
melissa <melissa160@gmail.com>
2016-12-02 18:17:00 -0500

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentBox from './App.jsx';
-import './index.css';
-
-ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('app')
-);
